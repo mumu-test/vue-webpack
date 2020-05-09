ab3149763614286b2d6478534a2c1e171da0a862
mumu <mumu@foxmail.com>
2020-05-09 22:39:42 +0800

    04.webpack 手动打包

--- 02.webpack/src/index.js
+++ 02.webpack/src/index.js
@@ -1,10 +1,27 @@
 import $ from "jquery";
+import './css/index.css'
+import './css/index.less'
+import './css/index.scss'
 $(function () {
     //隔行变色
     // $("li:odd").css("background", "cyan");
     // $("li:even").css("background", "pink");
     //三行隔行变色
-    $("li:nth-child(3n-2)").css("background", "cyan"); //1,4,7
+    $("li:nth-child(3n-2)").css("background", "pink"); //1,4,7
     $("li:nth-child(3n-1)").css("background", "skyblue"); //2,5,8
     $("li:nth-child(3n)").css("background", "lightblue"); //3,6,9
+})
+class Person {
+    static info = 'aaa'
+}
+
+console.log(Person.info)
+
+//--------------------------------------
+import Vue from 'vue'
+// 导入单文件组件
+import App from './components/App.vue'
+const vm = new Vue({
+    el: "#app",
+    render: h => h(App)
 })
--- 02.webpack/webpack.config.js
+++ 02.webpack/webpack.config.js
@@ -11,6 +11,17 @@
 //                  默认会将dist/main.js 作为默认的打包输出js文件
 // 如果不想使用默认的入口/出口js文件，我们可以通过改变 webpack.config.js 来设置入口/出口的js文件，如下：
 const path = require("path");
+//导入包
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const vuePlugin = new VueLoaderPlugin();
+//创建对象
+const htmlPlugin = new HtmlWebpackPlugin({
+    //设置生成预览页面的模板文件
+    template: "./src/index.html",
+    //设置生成的预览页面名称
+    filename: "index.html"
+})
 module.exports = {
     mode: "development",
     //设置入口文件路径
@@ -20,6 +31,41 @@ module.exports = {
         //设置路径
         path: path.join(__dirname, "./dist"),
         //设置文件名
-        filename: "budle.js"
+        filename: "bundle.js"
+    },
+    // 添加plugins信息
+    plugins: [htmlPlugin, vuePlugin],
+    module: {
+        rules: [{
+                //test设置需要匹配的文件类型，支持正则
+                test: /\.css$/,
+                //use表示该文件类型需要调用的loader
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/,
+                //limit用来设置字节数，只有小于limit值的图片，才会转换
+                //为base64图片
+                use: "url-loader?limit=16940"
+            },
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                //exclude为排除项，意思是不要处理node_modules中的js文件
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: "vue-loader"
+            }
+        ]
     }
 }
